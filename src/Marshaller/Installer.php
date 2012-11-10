26ed947a68cd41f9426223fd4e5d0c1a1244ce7e
Matthias Molitor <matthias@matthimatiker.de>
2012-11-10 20:46:44 +0100

    added stub for installer class

---
+++ src/Marshaller/Installer.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * \Marshaller\Installer
+ *
+ * @category PHP
+ * @package Marshaller
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/PackageMarshaller
+ * @since 10.11.2012
+ */
+
+namespace Marshaller;
+
+/**
+ *
+ *
+ * @category PHP
+ * @package Marshaller
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/PackageMarshaller
+ * @since 10.11.2012
+ */
+ class Installer
+ {
+     
+ }
