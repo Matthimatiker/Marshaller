5ef287740394df98142ec09c69a93e5e82834b49
Matthias Molitor <matthias@matthimatiker.de>
2012-11-10 23:18:32 +0100

    added test case stub

---
+++ tests/Marshaller/InstallerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * \Marshaller\InstallerTest
+ *
+ * @category PHP
+ * @package Marshaller
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/PackageMarshaller
+ * @since 10.11.2012
+ */
+
+namespace Marshaller;
+
+/**
+ * Tests the installer.
+ *
+ * @category PHP
+ * @package Marshaller
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/PackageMarshaller
+ * @since 10.11.2012
+ */
+class InstallerTest extends \PHPUnit_Framework_TestCase
+{
+    
+}
